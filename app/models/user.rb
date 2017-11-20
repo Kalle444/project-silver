10270a44f916aaa6b16f04f18eab33a7be19f064
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-20 17:07:43 +0100

    validation and associations added to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  has_many :user_interests
-  has_many :friend_requests_as_young_user, class_name: 'User', foreign_key: 'young_user_id'
-  has_many :friend_requests_as_old_user, class_name: 'User', foreign_key: 'old_user_id'
+  has_many :user_interests, dependent: :destroy
+  has_many :friend_requests_as_young_user, class_name: 'User', foreign_key: 'young_user_id', dependent: :destroy
+  has_many :friend_requests_as_old_user, class_name: 'User', foreign_key: 'old_user_id', dependent: :destroy
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :young, presence: true, inclusion: { in: [true, false] }
+
 
 
   # Include default devise modules. Others available are:
@@ -9,3 +13,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+
+
