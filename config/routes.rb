b80c5dfe76cf10c1949581865b654ef83320b33f
Robin Zuschke <robin.l.zuschke@gmail.com>
2017-11-21 10:14:19 +0100

    Created the user_interests_controller and seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources(:user_interests, only: [:index, :new, :create, :destroy])
+
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting the database..."
+
+User.destroy_all
+Interest.destroy_all
+
+puts "Seeding the database"
+
+john = User.create(first_name: "John", last_name: "User", email: "john@gmail.com", password: "123456")
+bill = User.create(first_name: "Bill", last_name: "User", email: "bill@gmail.com", password: "123456")
+maria = User.create(first_name: "Maria", last_name: "User", email: "maria@gmail.com", password: "123456")
+
+swimming = Interest.create(name: "Swimming")
+chess = Interest.create(name: "Chess")
+gaming = Interest.create(name: "Gaming")
+
+UserInterest.create(user_id: john, interest_id: swimming)
+UserInterest.create(user_id: john, interest_id: chess)
+UserInterest.create(user_id: maria, interest_id: gaming)
+UserInterest.create(user_id: bill, interest_id: gaming)
+UserInterest.create(user_id: john, interest_id: swimming)
+
+puts "Seeding done"
