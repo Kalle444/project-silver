53930ec9e1aaca2ecdbbdac11d72fe84594c50dd
Jeremy Boom <jeremy.boom77@gmail.com>
2017-11-21 13:59:46 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :user_interests, dependent: :destroy
   has_many :friend_requests_as_young_user, class_name: 'FriendRequest', foreign_key: 'young_user_id', dependent: :destroy
   has_many :friend_requests_as_old_user, class_name: 'FriendRequest', foreign_key: 'old_user_id', dependent: :destroy
-  validates :last_name, presence: true
   validates :young, inclusion: { in: [true, false], allow_nil: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
