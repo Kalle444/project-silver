8fd18109e3c028f3aeb1dfa535b68018b18eee53
Robin Zuschke <robin.l.zuschke@gmail.com>
2017-11-21 13:30:47 +0100

    User interests can be added and displayed. Started to build the seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :user_interests, dependent: :destroy
-  has_many :friend_requests_as_young_user, class_name: 'User', foreign_key: 'young_user_id', dependent: :destroy
-  has_many :friend_requests_as_old_user, class_name: 'User', foreign_key: 'old_user_id', dependent: :destroy
+  has_many :friend_requests_as_young_user, class_name: 'FriendRequest', foreign_key: 'young_user_id', dependent: :destroy
+  has_many :friend_requests_as_old_user, class_name: 'FriendRequest', foreign_key: 'old_user_id', dependent: :destroy
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :young, presence: true, inclusion: { in: [true, false] }
