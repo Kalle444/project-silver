053784069530e108a75461133cf3cebbf5a0119b
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-21 13:04:33 +0100

    validatios for last and first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :friend_requests_as_young_user, class_name: 'FriendRequest', foreign_key: 'young_user_id', dependent: :destroy
   has_many :friend_requests_as_old_user, class_name: 'FriendRequest', foreign_key: 'old_user_id', dependent: :destroy
   validates :young, inclusion: { in: [true, false], allow_nil: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 
 
   # Include default devise modules. Others available are:
