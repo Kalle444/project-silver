f9a4afce40df8a030b5d2bb27455b990fcb3df98
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-21 11:47:05 +0100

    controllers and validations updated for profiles and friendrequest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   has_many :user_interests, dependent: :destroy
-  has_many :friend_requests_as_young_user, class_name: 'User', foreign_key: 'young_user_id', dependent: :destroy
-  has_many :friend_requests_as_old_user, class_name: 'User', foreign_key: 'old_user_id', dependent: :destroy
+  has_many :friend_requests_as_young_user, class_name: 'FriendRequest', foreign_key: 'young_user_id', dependent: :destroy
+  has_many :friend_requests_as_old_user, class_name: 'FriendRequest', foreign_key: 'old_user_id', dependent: :destroy
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :young, presence: true, inclusion: { in: [true, false] }
-
+  validates :young, inclusion: { in: [true, false], allow_nil: false }
 
 
   # Include default devise modules. Others available are:
