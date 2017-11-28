396038f399798c680716e90255ffcb4fc21d1f30
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-28 13:09:29 +0100

    validation added on request message

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -2,6 +2,7 @@ class FriendRequest < ApplicationRecord
   belongs_to :young_user, class_name: 'User', foreign_key: 'young_user_id'
   belongs_to :old_user, class_name: 'User', foreign_key: 'old_user_id'
 
+  validates :request_message, presence: true
   validates :request_message, length: { maximum: 300,
     too_long: "300 characters is the maximum allowed" }
 
