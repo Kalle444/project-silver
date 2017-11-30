2cd97049e8f00b8d0344f880f516dcfad52c62a3
Karl Bergstrom <karl.o.bergstrom@gmail.com>
2017-11-30 14:45:23 +0100

    intest boxes chosen are displayed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,6 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update]
 
   def index
-    # raise
     @location = params[:location]
     @radius = 20
     @interests = Interest.all
